0c9a942ba94640608a395702b48d18e47a441702
Song Yang <songyang1218@gmail.com>
2020-11-27 18:18:46 -0500

    Initial commit

---
+++ Assets/Scripts/AnimatorCOR.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Component that connects the COR skinning algorithm to the Unity engine
+/// </summary>
+[RequireComponent(typeof(Animator))]
+public class AnimatorCOR : MonoBehaviour
+{
+
+    public SkinnedMeshRenderer[] skinnedMeshRenderers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
